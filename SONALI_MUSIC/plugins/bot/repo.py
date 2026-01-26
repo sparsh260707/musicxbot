b486b0dc2b90d066f646f4b8e63a1351cd881f90
sparsh260707 <125281480+sparsh260707@users.noreply.github.com>
2026-01-26 21:10:52 +0530

    ᴄᴏᴅᴇ ʙᴏᴛ ɴᴇᴛᴡᴏʀᴋ

---
+++ SONALI_MUSIC/plugins/bot/repo.py
@@ -0,0 +1,45 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from SONALI_MUSIC import app
+from config import BOT_USERNAME
+from SONALI_MUSIC.utils.errors import capture_err
+import httpx 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+start_txt = """**
+<u>❃ ᴡєʟᴄσϻє ᴛᴏ ᴛєᴧϻ ᴋɪᴛᴛᴜ ʀєᴘσs ❃</u>
+ 
+✼ ʀєᴘᴏ ɪs ηᴏᴡ ᴘʀɪᴠᴧᴛє ᴅᴜᴅє 😌
+ 
+❉  ʏᴏᴜ ᴄᴧη мʏ ᴜsє ᴘᴜʙʟɪᴄ ʀєᴘσs !!  
+
+✼ || [˹ᴋɪᴛᴛᴜ ꭙ ʙᴏᴛѕ˼ 💞](https://t.me/+xxpR_3jfLFhkYjNl) ||
+ 
+❊ ʀᴜη 24x7 ʟᴧɢ ϝʀєє ᴡɪᴛʜσᴜᴛ sᴛσᴘ**
+"""
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+        [ 
+          InlineKeyboardButton("✙ ᴧᴅᴅ ϻє вᴧʙʏ ✙", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        [
+          InlineKeyboardButton("• ʜєʟᴘ •", url="https://t.me/KITTUU_UPDATES"),
+          InlineKeyboardButton("• 𝛅ᴜᴘᴘσʀᴛ •", url="https://t.me/+xxpR_3jfLFhkYjNl"),
+          ],
+[
+InlineKeyboardButton("• ϻᴧɪη ʙσᴛ •", url=f"https://t.me/aaruxmusic_bot"),
+
+        ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://files.catbox.moe/kbi6t5.jpg",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
