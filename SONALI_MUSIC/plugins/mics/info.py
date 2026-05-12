a0e990c45db642157a2e249065a30bc115169647
sparsh260707 <125281480+sparsh260707@users.noreply.github.com>
2026-05-12 22:03:01 +0530

    done

--- SONALI_MUSIC/plugins/mics/info.py
+++ SONALI_MUSIC/plugins/mics/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="✙ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✙", url=f"https://t.me/ShizukaXmusicprobot?startgroup=true"),
+        InlineKeyboardButton(text="✙ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✙", url=f"https://t.me/KashishMusicRobot?startgroup=true"),
     ],
 ]
 
