c8c264d160647a04f4f601496a58b82fbaf8c9c7
sparsh260707 <125281480+sparsh260707@users.noreply.github.com>
2026-05-12 22:24:58 +0530

    done

--- SONALI_MUSIC/plugins/tools/couples.py
+++ SONALI_MUSIC/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •",
-            url=f"https://t.me/Hena_robot?startgroup=true",
+            url=f"https://t.me/kashishmusicrobot?startgroup=true",
         ),
     ],
 ]
